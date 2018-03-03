aab54d7739ce2a5f79b3e137088a5a70af7e111e
DengSongsong <DengSongsong1010@163.com>
2018-03-03 19:45:47 +0800

    搭建项目环境并创建项目目录

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'lib-flexible/flexible'
+import 'normalize.css/normalize.css'
 
 Vue.config.productionTip = false
 

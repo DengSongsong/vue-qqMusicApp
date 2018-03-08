0ea1584fdcf1fb540a6389c87e6c10c1e55a064a
DengSongsong <DengSongsong1010@163.com>
2018-03-09 01:03:42 +0800

    获取相关列表数据

--- src/components/musicHall/musicHall.vue
+++ src/components/musicHall/musicHall.vue
@@ -3,9 +3,9 @@
     <div class="musicHall-content">
       <div>
         <!-- 轮播图 -->
-        <div v-if="musicHall.length" class="slider-wrapper">
+        <div v-if="carouselPic.length" class="slider-wrapper">
           <slider>
-            <div v-for="item in musicHall" :key="item.index">
+            <div v-for="item in carouselPic" :key="item.index">
               <a :href="item.linkUrl">
                 <img :src="item.picUrl" alt="" class="needsclick">
               </a>
@@ -13,7 +13,7 @@
           </slider>
         </div>
         <!-- 导航列表 -->
-        <div class="radio-list">
+        <div class="radio-list-wrapper">
           <radio-list></radio-list>
         </div>
     </div>
@@ -28,21 +28,50 @@ import RadioList from '@/components/radio-list/radio-list'
 export default {
   data() {
     return {
-      musicHall: []
+      // 轮播图
+      carouselPic: [],
+      // 个性电台
+      radioList: [],
+      // 推荐歌单
+      songList: []
     }
   },
   created() {
     // 获取轮播图数据
-    this._getMusicHall()
+    this._getCarouselPic()
+    // 个性电台数据
+    this._getRadioList()
+    // 推荐歌单数据
+    this._getRecommendSongList()
   },
   methods: {
     // 获取轮播图数据 异步
-    _getMusicHall() {
+    _getCarouselPic() {
       getMusicHall().then(res => {
         if(res.code === ERR_OK) {
           // console.log(res)
-          this.musicHall = res.data.slider
-          // console.log(this.musicHall);
+          this.carouselPic = res.data.slider
+          // console.log(this.carouselPic);
+        }
+      })
+    },
+    // 个性电台数据
+    _getRadioList() {
+      getMusicHall().then(res => {
+        if(res.code === ERR_OK) {
+          // console.log(res)
+          this.radioList = res.data.radioList[0]
+          // console.log(this.radioList);
+        }
+      })
+    },
+    // 推荐歌单数据
+    _getRecommendSongList() {
+      getMusicHall().then(res => {
+        if(res.code === ERR_OK) {
+          // console.log(res)
+          this.songList = res.data.songList
+          // console.log(this.songList);
         }
       })
     }
@@ -65,9 +94,9 @@ export default {
       .slider-wrapper
         width 100%
         overflow hidden
-      .radio-list
+      .radio-list-wrapper
         position fixed
         z-index 1
         margin-top -.066667rem /* 5/75 */
-        margin-left .106667rem /* 8/75 */
+        margin-left .213333rem /* 16/75 */
 </style>

4f269cf7febb916e6953c7da5f17d9a3ba1576f2
DengSongsong <DengSongsong1010@163.com>
2018-03-10 01:34:26 +0800

    完成个性推荐新歌布局

--- src/components/musicHall/musicHall.vue
+++ src/components/musicHall/musicHall.vue
@@ -16,6 +16,28 @@
         <div class="radio-list-wrapper">
           <radio-list></radio-list>
         </div>
+        <div class="newRecommend-wrapper">
+          <div class="personality-radio">
+            <img :src="radioList.picUrl" alt="">
+            <span class="icon iconfont icon-bofang"></span>
+            <span class="radio-mame">个性电台</span>
+          </div>
+          <div class="newRecommend-list">
+            <div class="recommend-song">
+              <span class="recommend-title">新歌推荐</span>
+              <span class="recommend-decs">妈妈木陪你春暖花开</span>
+              <img src="../../common/image/newSong.jpg" alt="">
+            </div>
+            <div class="recommend-album">
+              <span class="recommend-title">数字专辑</span>
+              <span class="recommend-decs">310“春”日快乐</span>
+              <img src="../../common/image/newAlbum.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-titel">为你推荐歌单</h1>
+        </div>
     </div>
     </div>
   </div>
@@ -71,32 +93,85 @@ export default {
         if(res.code === ERR_OK) {
           // console.log(res)
           this.songList = res.data.songList
-          // console.log(this.songList);
+          console.log(this.songList);
         }
       })
     }
   },
   components: {
     Slider,
-    RadioList
+    RadioList,
   }
 }
 </script>
 <style lang="stylus">
+  @import "../../common/stylus/variable";
   .musicHall
     position fixed
     top 2.453333rem /* 184/75 */
     width 100%
     bottom 0
     .musicHall-content
+      position relative
       height 100%
       overflow hidden
       .slider-wrapper
         width 100%
         overflow hidden
       .radio-list-wrapper
-        position fixed
+        position relative
         z-index 1
-        margin-top -.066667rem /* 5/75 */
-        margin-left .213333rem /* 16/75 */
+        margin-top -.133333rem /* 10/75 */
+      .newRecommend-wrapper
+        width 9.573333rem /* 718/75 */
+        display flex
+        margin 0 auto
+        margin-top .533333rem /* 40/75 */
+        .personality-radio
+          position relative
+          width 3.36rem /* 252/75 */
+          height 3.36rem /* 252/75 */
+          flex 0 0 3.36rem /* 252/75 */
+          margin-right .346667rem /* 26/75 */
+          font-size 0
+          // text-align center
+          img
+            width 100%
+            display inline-block
+          .icon
+            position absolute
+            font-size $font-size-large-x
+            top: 45%;
+            left 42%
+          .radio-mame
+            font-size $font-size-medium-x
+            position absolute
+            left .693333rem /* 52/75 */
+            bottom .24rem /* 18/75 */
+            color $color-text
+        .newRecommend-list
+          flex 1
+          .recommend-song
+            margin-bottom .133333rem /* 10/75 */
+          .recommend-song, .recommend-album
+            width 100%
+            position relative
+            box-sizing border-box
+            height 1.6rem /* 120/75 */
+            padding .373333rem 0 .373333rem .373333rem
+            // padding-right .213333rem /* 16/75 */
+            background #334753
+            .recommend-title 
+              font-size $font-size-medium-x
+            .recommend-decs
+              display block
+              margin-top .106667rem /* 8/75 */
+              color $color-text-ll
+            img
+              position absolute
+              display inline-block
+              height 100%
+              right 0
+              top 0
+        
 </style>
